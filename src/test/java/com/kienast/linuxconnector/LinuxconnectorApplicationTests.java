f0689e604e88d9887a61d95af96ab327f303c044
Lukas Kienast <lukas.kienast@nowconsultians.at>
2020-07-12 14:05:29 +0200

    Initial commit

---
+++ src/test/java/com/kienast/linuxconnector/LinuxconnectorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kienast.linuxconnector;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LinuxconnectorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

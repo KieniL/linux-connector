2a5a1f30fb00fafdcfa694019c9242213602e9e7
Lukas Kienast <lukas.kienast@nowconsultians.at>
2020-07-16 20:19:36 +0200

    Implemented AuthApi

---
+++ apiservice/src/main/java/com/kienast/apiservice/security/SecurityConfiguration.java
@@ -0,0 +1,23 @@
+package com.kienast.apiservice.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(securedEnabled = true)
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter  {
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.csrf().disable()
+		.cors().and()
+		.authorizeRequests()
+		.antMatchers(HttpMethod.GET, "/").permitAll();
+
+	}
+}

71b7fd8ba34a7f2c2831ddaa27aa747412bc9038
Lukas Kienast <lukaskienast0@gmail.com>
2020-12-02 13:52:16 +0100

    Changed shellscript

--- dockerbuild.sh
+++ dockerbuild.sh
@@ -1,5 +1,5 @@
-mvn -f ./backend/authservice/pom.xml compile jib:dockerBuild
-mvn -f ./backend/connectorservice/pom.xml compile jib:dockerBuild
-mvn -f ./backend/apiservice/pom.xml compile jib:dockerBuild
+mvn -f ./backend/authservice/pom.xml compile jib:dockerBuild -Dimage=authservice
+mvn -f ./backend/connectorservice/pom.xml compile jib:dockerBuild -Dimage=connectorservice
+mvn -f ./backend/apiservice/pom.xml compile jib:dockerBuild -Dimage=apiservice
 
 docker build -t frontend:1.00 ./frontend

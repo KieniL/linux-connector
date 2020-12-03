274001dc417099c8d4d68ce26bb31ebbc45294e3
Lukas Kienast <lukaskienast0@gmail.com>
2020-12-03 15:01:33 +0100

    Changed tags to latest

--- dockerbuild.sh
+++ dockerbuild.sh
@@ -2,4 +2,4 @@ mvn -f ./backend/authservice/pom.xml compile jib:dockerBuild -Dimage=authservice
 mvn -f ./backend/connectorservice/pom.xml compile jib:dockerBuild -Dimage=connectorservice
 mvn -f ./backend/apiservice/pom.xml compile jib:dockerBuild -Dimage=apiservice
 
-docker build -t frontend:1.00 ./frontend
+docker build -t frontend:latest ./frontend
